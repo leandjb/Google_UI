89f6438482012fa7ef07b4205c4512faef5f51c2
leandjb <leandjb@gmail.com>
2022-04-08 00:00:14 -0500

    Second Commit

--- src/main/java/pages/GooglePage.java
+++ src/main/java/pages/GooglePage.java
@@ -3,16 +3,24 @@ package pages;
 public class GooglePage extends BasePage{
 
     private String searchButton = "(//input[@name='btnK'])[2]";
+    private String searchTextField = "";
 
-//    public GooglePage(){
-//        super(driver);
-//    }
-//
-//    public void navigateToGoogle(){
-//        navigateTo("https://www.google.com/");
-//    }
-//
-//    public void clickGoogleSearch(){
-//        clickElement(searchButton);
-//    }
+//todo resolver conexion con webdriver
+
+    public GooglePage(){
+        super(driver);
+    }
+
+    public void navigateToGoogle(){
+        navigateTo("https://www.google.com/");
+    }
+
+    public void clickGoogleSearch(){
+        clickElement(searchButton);
+    }
+
+    public void enterSearchCriteria(String criteria){
+        write(searchTextField, criteria);
+
+    }
 }
--- src/main/java/steps/GoogleSteps.java
+++ src/main/java/steps/GoogleSteps.java
@@ -4,15 +4,20 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.GooglePage;
 
 public class GoogleSteps {
 
+    GooglePage google = new GooglePage();
+
     @Given("^i am Google Search Page$")
     public void iAmGoogleSearchPage() {
+        google.navigateToGoogle();
     }
 
     @When("^i enter search criteria$")
     public void iEnterSearchCriteria() {
+        google.enterSearchCriteria("ASUS");
     }
 
     @And("^click on search button$")
